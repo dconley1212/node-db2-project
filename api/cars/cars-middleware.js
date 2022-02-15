1636c960144b95d151014ea8137579c5ead4448c
David Conley <dconley1989@gmail.com>
2022-02-15 15:04:13 -0700

    finished models and middleware errors and the schema

--- api/cars/cars-middleware.js
+++ api/cars/cars-middleware.js
@@ -1,15 +1,58 @@
+const { OPEN_READWRITE } = require("sqlite3");
+const { getAll } = require("./cars-model");
+
 const checkCarId = (req, res, next) => {
-  // DO YOUR MAGIC
-}
+  const { id } = req.params;
+  if (!id) {
+    next({ status: 404, message: `car with id ${id} is not found` });
+  } else {
+    next();
+  }
+};
 
 const checkCarPayload = (req, res, next) => {
-  // DO YOUR MAGIC
-}
+  const { vin, make, model, mileage } = req.body;
+  if (!vin) {
+    next({ status: 400, message: `${vin} is missing` });
+  } else if (!make) {
+    next({ status: 400, message: `${make} is missing` });
+  } else if (!model) {
+    next({ status: 400, message: `${model} is missing` });
+  } else if (!mileage) {
+    next({ status: 400, message: `${mileage} is missing` });
+  } else {
+    next();
+  }
+};
 
 const checkVinNumberValid = (req, res, next) => {
-  // DO YOUR MAGIC
-}
+  const { vin } = req.body;
+  if (typeof vin !== "string") {
+    next({ status: 400, message: `vin ${vin} is invalid` });
+  } else {
+    next();
+  }
+};
 
-const checkVinNumberUnique = (req, res, next) => {
-  // DO YOUR MAGIC
-}
+const checkVinNumberUnique = async (req, res, next) => {
+  const { vin } = req.body;
+  try {
+    const cars = await getAll();
+    cars.forEach((car) => {
+      if (car.vin === vin) {
+        next({ status: 400, message: `vin ${vin} already exists` });
+      } else {
+        next();
+      }
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = {
+  checkCarId,
+  checkCarPayload,
+  checkVinNumberValid,
+  checkVinNumberUnique,
+};
--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,16 @@
 // DO YOUR MAGIC
+const express = require("express");
+// const db = require("../../data/db-config");
+const server = require("../server");
+
+const router = express.Router();
+
+server.use((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    custom: "something went wrong",
+    message: err.message,
+    stack: err.stack,
+  });
+});
+
+module.exports = router;
