8bcaca148a47f54e66470ae92c0cd5ae6e488f51
David Conley <dconley1989@gmail.com>
2022-02-15 15:24:34 -0700

    finished the first two crud requests but need to test them all

--- api/cars/cars-model.js
+++ api/cars/cars-model.js
@@ -1,11 +1,26 @@
+const dbConfig = require("../../data/db-config");
+
 const getAll = () => {
   // DO YOUR MAGIC
-}
+  return dbConfig("cars");
+};
 
-const getById = () => {
+const getById = async (id) => {
   // DO YOUR MAGIC
-}
+  const carById = await dbConfig("cars").where("id", id).first();
 
-const create = () => {
+  return carById;
+};
+
+const create = async (id, car) => {
   // DO YOUR MAGIC
-}
+  const createNewPost = await dbConfig("cars").insert(car);
+  const newPost = await getById(createNewPost);
+  return newPost;
+};
+
+module.exports = {
+  getAll,
+  getById,
+  create,
+};
--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,11 +1,35 @@
 // DO YOUR MAGIC
 const express = require("express");
 // const db = require("../../data/db-config");
-const server = require("../server");
+const Cars = require("./cars-model");
+const {
+  checkCarId,
+  checkCarPayload,
+  checkVinNumberValid,
+  checkVinNumberUnique,
+} = require("./cars-middleware");
 
 const router = express.Router();
 
-server.use((err, req, res, next) => {
+router.get("/", (req, res, next) => {
+  Cars.getAll()
+    .then((cars) => {
+      res.status(200).json(cars);
+    })
+    .catch(next);
+});
+
+router.get("/:id", checkCarId, async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const car = await Cars.getById(id);
+    res.status(200).json(car);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.use((err, req, res, next) => {
   res.status(err.status || 500).json({
     custom: "something went wrong",
     message: err.message,
